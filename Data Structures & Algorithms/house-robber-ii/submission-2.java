89a0fd4be51d8e09c8f1b5b7a7253bc7671142da
JunSeo <37623843+jun562@users.noreply.github.com>
2026-09-05 22:18:49 +0900

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int rob(int[] nums) {
+        int N = nums.length;
+        int[] dp1 = new int[N];
+        int[] dp2 = new int[N];
+
+        if(N == 1){
+            return nums[0];
+        }
+
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(nums[0],nums[1]);
+
+        dp2[0] = 0;
+        dp2[1] = nums[1];
+
+        for(int i = 2; i < N - 1; i++){
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + nums[i]);
+        }
+
+        for(int i = 2; i < N; i++){
+            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + nums[i]);
+        }
+
+        return Math.max(dp1[N-2], dp2[N-1]);
+    }
+}
+
+//i번째 까지 집의 훔칠 수 있는 최댓값
+//마지막 집 제외
+//첫번째 집 제외
